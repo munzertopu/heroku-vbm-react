b1801a947f74e78e1e66e27dc59d5ba6c9fe8835
unknown <munzertopu@gmail.com>
2020-03-09 18:49:09 +0600

    order seed weeks open

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ad1cbada7ab871daf867a05949fbd13.js"
+  "/precache-manifest.dc832cee43b8c820c378b328c72bd871.js"
 );
 
 self.addEventListener('message', (event) => {
